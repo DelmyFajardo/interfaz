f79a7a8f664eebadb5892c50671088c06da33c6f
delmy <dfajardob@miumg.edu.gt>
2024-04-12 16:42:18 -0600

    notificaciones

---
+++ desafio2/Notificacion.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace desafio2
+{
+   public class Notificacion
+    {
+        public string De { get; set; }
+        public string Mensaje { get; set; }
+        public Notificacion(string de, string mensaje)
+        { 
+            De = de;
+            Mensaje = mensaje;
+        }
+    }
+
+    public class Email : Notificacion , INotificacion
+    { 
+        public Email (string correo, string de, string asunto,string mensaje) : base(de,mensaje)
+        {
+            Correo = correo;
+            Asunto = asunto;
+        }
+        public string Correo { get; set; }
+        public string Asunto { get; set; }
+        public void notifica()
+        {
+            Console.WriteLine("Tienes una  notificacion nueva de: " +De);
+            NotificacionEmail();
+
+        }
+        public void NotificacionEmail()
+        {
+     
+            Console.WriteLine($"Correo : {Correo}");
+            Console.WriteLine($"Asunto : {Asunto}");
+            Console.WriteLine($"Mensaje : {Mensaje}");
+        }
+    }
+
+    public class whats : Notificacion, INotificacion
+    {   
+      public whats (string de, string mensaje, int numeroDeTelefono) : base(de, mensaje)
+        {
+            NumeroDeTelefono = numeroDeTelefono;
+                
+        }
+
+      private int NumeroDeTelefono { get; set; }
+        public void notifica()
+        {
+            Console.WriteLine("Tienes una notificacion nueva de: "+ De);
+            NotificacionWhatsap();
+
+        }
+        public void NotificacionWhatsap()
+        {
+           
+            Console.WriteLine($"Numero :{NumeroDeTelefono}");   
+            Console.WriteLine($"Mensaje : {Mensaje}");
+        }
+    }
+    public class sms : Notificacion, INotificacion
+    {
+        public sms(string de, string mensaje, int numeroDeTelefono) : base(de, mensaje)
+        {
+            NumeroDeTelefono = numeroDeTelefono;
+        }
+        private int NumeroDeTelefono { get; set; }
+        public void notifica()
+        {
+            Console.WriteLine("Tienes una notificacion nueva de: "+ De);
+            NotificacionSMS();
+        }
+        public void NotificacionSMS()
+        {
+            Console.WriteLine($"Numero :{NumeroDeTelefono}");
+            Console.WriteLine($"Mensaje : {Mensaje}");
+        }
+    }
+
+    public interface INotificacion
+    {
+        void notifica();
+    }
+    
+
+   
+
+}
---
+++ desafio2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string Correo = "pruebamary358@gmail.com ";
+            string Asunto = "Importancia ";
+            string De = "Maria";
+            string Mensaje = "Necesito estudiar.  ";
+            int NumeroDeTelefono = 12453267;
+
+            Email email = new Email(Correo, De, Asunto, Mensaje);
+            Console.WriteLine("Informacion de Email"); 
+            email.notifica();
+            Console.WriteLine("");
+
+            whats Whats = new whats(De,Mensaje,NumeroDeTelefono);
+            Console.WriteLine("Notificacion de whatsap");
+            Whats.notifica();
+            Console.WriteLine("");
+
+            sms Sms = new sms(De, Mensaje, NumeroDeTelefono);
+            Console.WriteLine("Notificacion de SMS");
+            Sms.notifica();
+            Console.WriteLine("");
+
+
+            Console.ReadLine();
+        }
+
+    }
+
+   
+}
