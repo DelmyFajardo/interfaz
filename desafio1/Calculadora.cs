3154915466a98fb14c4a0459ffebb286a53ebb89
delmy <dfajardob@miumg.edu.gt>
2024-04-12 16:41:54 -0600

    Calculadora cientifica

---
+++ desafio1/Calculadora.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio1
+{
+    public class Calculadora
+    {
+        public string Marca { get; set; }
+        public string Serie { get; set; }
+        public Calculadora(string marca, string serie)
+        {
+            Marca = marca;
+            Serie = serie;
+        }
+        public double Sumar (double n1, double n2)
+        {
+            return n1 + n2;
+        }
+        public double Restar (double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public double Multiplicacion (double n1, double n2)
+        {
+            return n1 * n2;
+        } 
+        public double Division (double n1, double n2)
+        {
+            return n1 / n2;
+
+        }
+
+        public string Tipo()
+        {
+            return "Calculadora basica";
+                
+        }
+    }
+
+    public class CalculadoraCientifica : Calculadora
+    {
+        public CalculadoraCientifica(string marca,string serie ): base (marca, serie )
+        {
+
+        }
+        public double Potencias(double n1,double n2)
+        {
+            return Math.Pow(n1, n2);
+
+        }
+        public double RaizCuadrada(double n1)
+        {
+            return Math.Sqrt(n1);
+        }
+        public int Modulo(int n3, int n4)
+        {
+            return  n3 % n4;
+       }
+
+        public double Logaritmo(double n1, double n2)
+        {
+            return Math.Log10((double)n1);
+
+        }
+        public virtual string Tipo()
+        {
+            return "Cientifica";
+        }
+    }
+
+}
---
+++ desafio1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 10;
+            double n2 =3;
+            int n3= 17;
+            int  n4 = 3;
+            Calculadora Calc = new Calculadora("Casio", "fx-899");
+            double total = Calc.Sumar (n1,n2); 
+            Console.WriteLine( "la suma es: "+total);
+          
+            // resta
+            double diferencia = Calc.Restar(n1, n2);
+            Console.WriteLine("la diferencia es: "+diferencia);
+            
+            //multiplicacion
+            double multiplicar = Calc.Multiplicacion(n1, n2);
+            Console.WriteLine("la multiplicacion es: "+ multiplicar);
+
+            double division = Calc.Division(n1, n2);
+            Console.WriteLine("la dicision es: "+division);
+
+            CalculadoraCientifica cientifica = new CalculadoraCientifica("saso", "Gb-167");
+            double pontencia = cientifica.Potencias(n1, n2);
+            Console.WriteLine("la potencia es:"+pontencia);
+
+            double Raiz = cientifica.RaizCuadrada(n1);
+            Console.WriteLine("la raiz de:"+n1 +" es:" +Raiz);
+
+            int modulo = cientifica.Modulo(n3,n4);
+            Console.WriteLine("el modulo  es:" + modulo);
+
+            double logari = cientifica.Logaritmo(n1, n2);
+            Console.WriteLine("el logaritmo es:"+ logari);
+
+            MostrarTipo(Calc);
+            MostrarTipo(cientifica);
+
+
+            Console.ReadLine();
+        }
+        public static void MostrarTipo(Calculadora calculadora)
+        {
+            Console.WriteLine(calculadora.Tipo());
+        }
+    }
+}
